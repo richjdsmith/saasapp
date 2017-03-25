c766f24d76b14b5e8a42ba71a9a4e9c4b7abb893
richjdsmith <rich@richjdsmith.ca>
2017-03-24 20:03:58 -0700

    Styled index page to show all profiles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,14 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
 
+  def index
+    @users = User.includes(:profile)
+
+  end
   # GET users/:id
   def show
     @user = User.find( params[:id] )
   end
+
 
 end
