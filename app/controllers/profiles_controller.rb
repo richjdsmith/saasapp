758e5376752f0cdec104a879bf52b61da6c82135
richjdsmith <rich@richjdsmith.ca>
2017-03-24 18:05:24 -0700

    Added before filters to lock down certain sections of site for correct user

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,6 @@
 class ProfilesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :only_current_user
 
   # GET to /user/:user_id/profile/new
   def new
@@ -44,9 +46,12 @@ class ProfilesController < ApplicationController
   end
 
 
-private
-  def profile_params
-    params.require(:profile).permit(:first_name, :last_name, :contact_email, :phone_number, :job_title, :description, :avatar)
-  end
-
+  private
+    def profile_params
+      params.require(:profile).permit(:first_name, :last_name, :contact_email, :phone_number, :job_title, :description, :avatar)
+    end
+    def only_current_user
+      @user = User.find( params[:user_id])
+      redirect_to(root_url) unless @user == current_user
+    end
 end
